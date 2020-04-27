a1f5ff6dd3b5100c6d1f8cab98026f4ee69f03e8
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-04-27 14:32:53 -0500

    Creando la vista y el controlador para el login

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,8 +76,8 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
+	'username' => 'root',
+	'password' => 'PruebaTecnica',
 	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
